6471f29903577b8e75b2b5cd76b8a759b66bd2ee
mingmuchuan <1871998048@qq.com>
2020-12-16 16:14:15 +0800

    vercel

---
+++ nuxt.config.js
@@ -0,0 +1,9 @@
+export default {
+  ssr: true // 默认是服务端渲染模式
+
+  // 关闭服务端渲染模式，走客户端渲染
+  // ssr: false
+
+  // 生成纯静态网站
+  // target: 'static'
+}
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Hello world!</h1>
+    <p>
+      <nuxt-link to="/about">Go About</nuxt-link>
+    </p>
+    <ul>
+      <li
+        v-for="post in posts"
+        :key="post.id"
+      >{{ post.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData () {
+    const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    return {
+      posts: data
+    }
+  }
+}
+</script>
